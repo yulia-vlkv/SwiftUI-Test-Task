91f879db2a9a46c5cb040c22e0fb0e6d94e046b1
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-08-30 10:00:59 +0200

    Initial Commit

---
+++ SwiftUI_Test_Task/SwiftUI_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Test_TaskApp.swift
+//  SwiftUI_Test_Task
+//
+//  Created by Iuliia Volkova on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
